12fb6b71919adc648d7afaeb120319152a62be51
Myv-hs <matthewcoyle@rubberduckynetwork.com>
2017-07-14 22:26:22 +0200

    0.9.2

--- assets/javascript/contentdisplay.js
+++ assets/javascript/contentdisplay.js
@@ -1,9 +1,20 @@
 'use strict';
 $('.content').hide();
+
+var urlSelector = window.location.hash;
+var urlSelector = urlSelector.split("#")[1];
+console.log(urlSelector);
+if (urlSelector) {
+	$('.'+urlSelector).slideDown(1000, function() {
+		$('html, body').animate({scrollTop: 400}, 200);
+	});
+}
+
 $(function() {
 	$('.content-icon').on('click', function() {
 		var chosencontent = $(this).attr('data-title');
 		if (!$('.'+chosencontent).is(":visible")) {
+			window.location.hash = chosencontent;
 			$('.content').slideUp();			
 			$('.'+chosencontent).slideDown(1000, function() {
 				$('html, body').animate({scrollTop: 400}, 200);

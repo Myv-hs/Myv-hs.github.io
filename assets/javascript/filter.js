e8b412e16f9bbcc806286849800062b63589d643
Myv-hs <matthewcoyle@rubberduckynetwork.com>
2017-07-22 20:39:16 +0200

    0.9.3

--- assets/javascript/filter.js
+++ assets/javascript/filter.js
@@ -2,7 +2,7 @@
 
 $(function() {
 	$('.filter').on('click', function() {
-		var chosenfilter = $(this).attr('data-filter');
+		chosenfilter = $(this).attr('data-filter');
 		$('.hidable').hide();
 		$('.'+chosenfilter).show();
 	});

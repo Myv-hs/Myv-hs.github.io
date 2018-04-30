88459149bb692ed9bfbb2bbc230efce67b147c88
Myv-hs <matthewcoyle@me.com>
2018-04-30 15:36:07 +0200

    index grid port fix

--- assets/javascript/contentdisplay.js
+++ assets/javascript/contentdisplay.js
@@ -6,7 +6,7 @@ $('.content').hide(0, function(){
 	console.log("contentdisplay.js :: "+urlSelector);
 	if (urlSelector) {
 		$('.'+urlSelector).slideDown(1000, function() {
-			$('html, body').animate({scrollTop: 400}, 200);
+			//$('html, body').animate({scrollTop: 400}, 200);
 		});
 	}
 });
@@ -18,11 +18,11 @@ $('.content').hide(0, function(){
 			window.location.hash = chosencontent;
 			$('.content').slideUp();			
 			$('.'+chosencontent).slideDown(1000, function() {
-				$('html, body').animate({scrollTop: 400}, 200);
+				//$('html, body').animate({scrollTop: 400}, 200);
 			});
 		} else {
 			$('.content').slideUp(1000, function() {
-				$('html, body').animate({scrollTop: 0}, 200);
+				//$('html, body').animate({scrollTop: 0}, 200);
 			});
 		}
 	});
